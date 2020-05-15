ca0d1918888fcba1de104bd400d548feb339c42f
atla5 <aidans@bu.edu>
2020-05-15 11:57:26 -0400

    prod: deploy script, 'ignore_prod.cms' for /library/firstyear hosting

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# show errors
+set -e
+
+# prepare sftp command
+user=aidans
+server=webdev.bu.edu
+dev_path=/Users/aidans/projects/bulib.github.io/
+prod_path=/web/l/i/library/firstyear/
+
+# run the sftp commands
+eval "sftp $user@$server <<EOF
+cd $prod_path
+pwd
+ls
+put -r $dev_path/*
+ls
+exit
+EOF"
+
+echo "completed deployment"  
